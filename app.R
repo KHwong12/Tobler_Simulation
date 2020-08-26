460d1e9c7167225e14893a6fd2fc47502ecb9dc8
Kenneth Wong <khwongk12@gmail.com>
2020-08-26 23:47:39 +0800

    Changed input parameter of hiking function from max_speed to flat_terrain_speed

--- app.R
+++ app.R
@@ -20,56 +20,45 @@ ui <- fluidPage(
     sidebarLayout(
         sidebarPanel(
             sliderInput("speed",
-                        "Walking Speed",
+                        "Walking speed on flat surface (km/hr)",
                         min = 0.1,
-                        max = 10,
-                        value = 6)
+                        max = 8,
+                        value = 5)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-            plotlyOutput("distPlot")
+            plotlyOutput("toblerPlot")
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output, ...) {
-
-    # output$distPlot <- renderPlot({
-    # 
-    # 
-    #     plot <- ggplot(data.frame(x = c(-60, 60)), aes(x)) +
-    #         stat_function(fun = function(x) toblers_hiking_function(slope_angle = x, max_speed = input$speed), n = 600) +
-    #         ylim(0,6) +
-    #         theme_minimal()
-    #     
-    #     plot
-    # 
-    # })
-    
-    
-    
-
-    
-    
-    output$distPlot <- renderPlotly({
+  
+    output$toblerPlot <- renderPlotly({
 
         req(input$speed)
         
-        
+        speed_flat_terrain = toblers_hiking_function(0, flat_terrain_speed = input$speed)
         
         dummydata <- data.frame(slope = seq(-60, 60, by = .2)) %>%
-            mutate(speed = toblers_hiking_function(slope_angle = slope, max_speed = input$speed))
+            mutate(speed = toblers_hiking_function(slope_angle = slope, flat_terrain_speed = input$speed))
+
+        tobler_plot <- ggplot(dummydata, aes(x = slope, y = speed)) +
+            geom_line() +
+            geom_hline(yintercept = speed_flat_terrain, color = "red") +
+            ylim(0,10)
+        
+        
+        # should use geom_function instead
+        # TODO: how to add shiny interactive input to geom_function()?
+        # ggplot() + xlim(-60, 60) +
+        #     geom_function(fun = toblers_hiking_function, n = 600)
         
 
-        
-        # tobler_plot <- ggplot(dummydata, aes(x = slope, y = speed)) +
-        #     geom_line()
-        # 
-        # ggplotly(tobler_plot)
-        
-        plot_ly(dummydata, x = ~slope, y = ~speed) %>% add_lines()
+        ggplotly(tobler_plot)
+
     })
     
 }
