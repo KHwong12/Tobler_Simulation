1cfe5a3dd898c75cfcc5583b2b501bd6bff42e79
Kenneth Wong <khwongk12@gmail.com>
2021-04-23 21:12:13 +0800

    Revert "Move description paragraphs to separate document"
    This reverts commit 5700db3219c78d135212fe2f9998f999ca39eb95.

--- app.R
+++ app.R
@@ -38,8 +38,45 @@ ui <- fluidPage(
 
     column(
       5,
+
+      strong(p("NOTE: This application is still in development.")),
+
+      p("This interactive web application allows you to interact and check how walking speed varies with slope of the path according to Tobler's hiking function.
+        This function serves the basis of Walking Accessibility Assessment Tool (WAAT) developed by",
+        tags$a("Tang et. al (2020)", href = "https://doi.org/10.1177/2399808320932575"),
+        "."),
       
-      htmltools::includeMarkdown("app-description.md"),
+
+      # Inline math symbols needs to wrap with withMathJax() function
+      # https://shiny.rstudio.com/gallery/mathjax.html
+            
+      withMathJax(p(tags$a("Tobler's hiking function", href = "https://en.wikipedia.org/wiki/Tobler%27s_hiking_function"),
+                    "is an exponential function determining walking speed, taking into account the slope angle.",
+                    "The walking speed $W$ could be expressed as:")),
+
+
+      # Render equation ------------------
+      # double backslash (instead of single) since first backslash will be truncated
+
+      p("$$ W = Me^{-3.5 \\times \\left\\lvert tan \\theta + 0.05 \\right\\rvert} $$"),
+
+      withMathJax(p("Where $\\theta$ is the slope and $M$ is maximum walking speed.")),
+
+      br(),
+
+      h3("How to Use"),
+
+      # Require to add tags$ for list
+      # https://shiny.rstudio.com/articles/tag-glossary.html
+
+      tags$ol(
+        tags$li("Drag the slider to change the walking speed on flat ground."),
+        tags$li("Move along the plot on the right to observe the relationship."),
+        tags$li("Check the walking time for the selected paths below.")
+      ),
+      
+      p("For reference, ground speed of a normal adult is 5 km/hr. Older adults who are healthy generally will have ground walking speed around 3.24 - 4.68 km/hr."),
+
       br()
     ),
 
